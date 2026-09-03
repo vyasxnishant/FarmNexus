c06cb35ff439e364f569af5502b68a1e23d34761
Nishant <nishant@farmnexus.local>
2026-09-03 07:06:02 +0530

    adding vercel json

---
+++ api/index.js
@@ -0,0 +1,9 @@
+﻿import app from '../backend/dist/index.js'
+
+export default function handler(req, res) {
+  const targetUrl = req.headers['x-matched-path'] || req.originalUrl || req.url
+  if (targetUrl && (req.url === '/api' || req.url === '/api/' || req.url === '/api/index.js' || req.url === '/api/index')) {
+    req.url = targetUrl
+  }
+  return app(req, res)
+}
