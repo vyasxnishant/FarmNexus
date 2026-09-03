9880a1ebb34e298c3d81021c89119f7bb9201ac6
Nishant <nishant@farmnexus.local>
2026-09-03 10:22:57 +0530

    Fix Netlify API routing

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http'
 import app from '../../src/index.js'
 
-export const handler = serverless(app)
+export const handler = serverless(app, {
+  requestId: 'x-nf-request-id'
+})
